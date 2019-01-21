09b03e1399603210b4b0b55b7f2b65c1f163d96e
Janine <Janine@192.168.0.15>
2019-01-20 21:11:51 -0300

    COmunicação com o mongDB

--- src/main/java/com/stone/aries/migrador/service/impl/StorageServiceImpl.java
+++ src/main/java/com/stone/aries/migrador/service/impl/StorageServiceImpl.java
@@ -1,11 +1,16 @@
-package com.stone.aries.migrador.service;
+package com.stone.aries.migrador.service.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.stone.aries.migrador.model.Formulario;
+import com.stone.aries.migrador.model.Pergunta;
+import com.stone.aries.migrador.service.StorageService;
 import com.stone.aries.migrador.service.excelreader.ExcelReader;
 import com.stone.aries.migrador.service.excelreader.planilha.Coluna;
 import com.stone.aries.migrador.service.excelreader.planilha.Linha;
@@ -15,7 +20,9 @@ import com.stone.aries.migrador.service.excelreader.planilha.Planilha;
 public class StorageServiceImpl implements StorageService {
 
 	@Override
-	public void store(MultipartFile file) throws InvalidFormatException, IOException {
+	public List<Formulario> store(MultipartFile file) throws InvalidFormatException, IOException {
+		List<Formulario> listaFormularios = new ArrayList<>();
+		List<Pergunta> listaPerguntas = new ArrayList<>();
 		
 		ExcelReader r = new ExcelReader();
 		Planilha planilha = r.loadPlanilha(file);
@@ -24,14 +31,22 @@ public class StorageServiceImpl implements StorageService {
 		System.out.println("\n");
 		while (planilha.existeProximaLinha()) {
 			Linha linha = planilha.proximaLinha();
-
+			
 			while (linha.existeProximaColuna()) {
 				Coluna c = linha.proximaColuna();
 				System.out.println("Header:" + c.getHeader());
 				System.out.println("Value:" + c.getValue());
+				listaPerguntas.add(new Pergunta(c.getHeader(), c.getValue()));
 			}
-			System.out.println();			
+			
+			if(listaPerguntas.size()>0) {
+				listaFormularios.add(new Formulario(listaPerguntas));
+				listaPerguntas = new ArrayList<>();
+			}
+			
+			System.out.println();
 		}
+		return listaFormularios;
 	}
 
 }
